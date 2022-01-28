790fa85d2d18411290bef8197a166df1615c98db
Tom <git@tomr.me>
2022-01-28 13:11:22 +0000

    begin work on estate agent properties page

---
+++ estate-agent/js/houses.js
@@ -0,0 +1,96 @@
+const houses = [{
+    "title": "Windsor House",
+    "type": "house",
+    "is_for": "purchase",
+    "rights": "Freehold",
+    "price": 825000,
+    "price_type": "Guide Price",
+    "beds": 8,
+    "images": [
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/55k/54640/85621438/54640_SBY190143_IMG_10_0000_max_476x317.jpg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/55k/54640/85621438/54640_SBY190143_IMG_11_0000_max_476x317.jpg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/55k/54640/85621438/54640_SBY190143_IMG_12_0000_max_476x317.jpg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/55k/54640/85621438/54640_SBY190143_IMG_15_0000_max_476x317.jpg"
+    ]
+},
+
+{
+    "title": "3 St Aulkmonds Square",
+    "type": "house",
+    "is_for": "purchase",
+    "rights": "Freehold",
+    "price": 450000,
+    "price_type": "Offers in Region Of",
+    "beds": 3,
+    "baths": 2,
+    "images": [
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/20k/19503/114082757/19503_30982537_IMG_00_0000_max_476x317.jpeg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/20k/19503/114082757/19503_30982537_IMG_01_0000_max_472x317.jpeg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/20k/19503/114082757/19503_30982537_IMG_02_0000_max_472x317.jpeg"
+    ]
+},
+
+{
+    "title": "Dogpole",
+    "type": "house",
+    "is_for": "purchase",
+    "rights": "Freehold",
+    "price": 450000,
+    "beds": 3,
+    "baths": 1,
+    "images": [
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/55k/54823/116922554/54823_SBY210108_IMG_00_0000_max_476x317.jpeg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/55k/54823/116922554/54823_SBY210108_IMG_01_0000_max_476x317.jpeg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/55k/54823/116922554/54823_SBY210108_IMG_02_0000_max_476x317.jpeg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/55k/54823/116922554/54823_SBY210108_IMG_03_0000_max_476x317.jpeg"
+    ]
+},
+
+{
+    "title": "Quarry Place",
+    "type": "flat",
+    "is_for": "purchase",
+    "rights": "Leasehold",
+    "price": 400000,
+    "beds": 1,
+    "baths": 1,
+    "images": [
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/55k/54823/113676026/54823_SBY210151_IMG_01_0000_max_476x317.jpeg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/55k/54823/113676026/54823_SBY210151_IMG_00_0000_max_476x317.jpeg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/55k/54823/113676026/54823_SBY210151_IMG_03_0000_max_476x317.jpeg"
+    ]
+},
+
+{
+    "title": "Watergage Mansions",
+    "type": "flat",
+    "is_for": "purchase",
+    "rights": "Leasehold",
+    "price": 320000,
+    "beds": 3,
+    "baths": 1,
+    "images": [
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/4k/3680/117402695/3680_31155224_IMG_00_0000_max_476x317.jpeg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/4k/3680/117402695/3680_31155224_IMG_01_0000_max_476x317.jpeg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/4k/3680/117402695/3680_31155224_IMG_02_0000_max_476x317.jpeg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/4k/3680/117402695/3680_31155224_IMG_07_0000_max_476x317.jpeg"
+    ]
+},
+
+{
+    "title": "1 Belmont Mansions",
+    "type": "flat",
+    "is_for": "purchase",
+    "rights": "Leasehold",
+    "price": 315000,
+    "price_type": "Offers in the region of",
+    "beds": 2,
+    "baths": 2,
+    "images": [
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/20k/19503/82831866/19503_31147188_IMG_00_0000_max_476x317.jpeg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/20k/19503/82831866/19503_31147188_IMG_01_0000_max_476x317.jpeg",
+        "https://media.rightmove.co.uk/dir/crop/10:9-16:9/20k/19503/82831866/19503_31147188_IMG_04_0000_max_476x317.jpeg"
+    ]
+}
+
+];
---
+++ estate-agent/js/sorting.js
@@ -0,0 +1,71 @@
+
+        reSort("weighted");
+
+        async function reSort(sortMethod) {
+            let sortedHouses = houses;
+            console.info("Sorting");
+
+            switch (sortMethod) {
+                case "low-to-high": {
+                    console.info("Low to High");
+                    sortedHouses.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+                    break;
+                }
+
+                case "high-to-low": {
+                    console.info("High to Low");
+                    sortedHouses.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+                    break;
+                }
+
+                case "weighted": {
+                    console.info("Weighted");
+                    sortedHouses.sort((a, b) => parseFloat(b.weight) - parseFloat(a.weight));
+                    break;
+                }
+
+
+                default: {
+                    // continue;
+                }
+            }
+
+            console.log(sortedHouses);
+
+            let outputHTML = "";
+
+            let houseID = 0;
+            for(const h of sortedHouses) {
+                houseID++;
+
+                let imageHTML = "";
+                let imageID = 0;
+
+                for(const img of h.images) {
+                    imageID++;
+
+                    imageHTML += `
+                        <img src="${img}" style="display: none;" id="${houseID}-${imageID}" onclick="cycleImage(this)"
+    
+                        >
+                    `
+                }
+
+                outputHTML += `
+                <li>
+                    <figure class="image-gallery">
+                        ${imageHTML}
+                    </figure>
+
+                    <h1 class="title">${h.title}</h1>
+                    <h2 class="stats">
+                    <i class="fas fa-fw fa-bed"></i> ${h.beds?h.beds:'?'} beds &bull;
+                    <i class="fas fa-fw fa-bath"></i> ${h.baths?h.baths:'?'} baths
+                    </h2>
+                    <h3>${h.type} to ${h.is_for},${h.price_type?` ${h.price_type}`:''} <b>£${h.price}</b>${h.rights?` (${h.rights})`:''}</h3>
+                </li>
+                `
+            }
+
+            document.getElementById('sorting').innerHTML = outputHTML;
+        }
